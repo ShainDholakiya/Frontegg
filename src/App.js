36e06067d08ccf610882aa02b1da39002264f0a0
ShainDholakiya <shainshain99@gmail.com>
2022-10-12 01:16:34 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { ContextHolder } from "@frontegg/rest-api";
+import {
+  AdminPortal,
+  useAuth,
+  useAuthActions,
+  useLoginWithRedirect,
+} from "@frontegg/react";
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+  const { switchTenant } = useAuthActions();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+
+  const handlePortalClick = () => {
+    AdminPortal.show();
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#F5F6F8] h-screen pt-5">
+      {isAuthenticated ? (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-auto">
+          <img className="w-full" src={user?.profilePictureUrl} alt={user?.name} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{user?.name}</div>
+            <p className="text-gray-700 text-base">Email: {user?.email}</p>
+            <p className="text-gray-700 text-base">Tenant: {user?.tenantId}</p>
+            <div>
+              <button
+                className="bg-gray-500 hover:bg-gray-700 text-white py-1 px-2 rounded"
+                onClick={() => {
+                  navigator.clipboard.writeText(user?.accessToken);
+                  alert("Access token copied to clipboard");
+                }}
+              >
+                Copy Access Token
+              </button>
+            </div>
+          </div>
+          <div className="px-6 py-4">
+            Switch Tenant:
+            <select
+              className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => switchTenant(e.target.value)}
+            >
+              {user?.tenantIds.map((tenantId, i) => (
+                <option key={tenantId} defaultValue={user?.tenantId}>
+                  {tenantId}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <button
+              onClick={handlePortalClick}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Open Admin Portal
+            </button>
+          </div>
+          <div className="absolute top-0 right-0 m-5">
+            <button
+              onClick={logout}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          <div className="flex flex-col items-center">
+            <img src={"https://frontegg.com/wp-content/uploads/2022/07/logo-black-1.svg"} className="App-logo" alt="logo" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-jy4fjzffdnda.frontegg.com',
+  clientId: '2f881264-38cf-4bb8-8e26-33a4420fd684'
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+        <App />
+    </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
