f7f334983b83b9a8f7e172ee845c6fb2f4345513
ShainDholakiya <shainshain99@gmail.com>
2022-10-12 02:23:00 -0700

    update to select bug

--- src/App.js
+++ src/App.js
@@ -52,11 +52,12 @@ function App() {
           <div className="px-6 py-4">
             Switch Tenant:
             <select
+              defaultValue={user?.tenantId}
               className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
               onChange={(e) => switchTenant(e.target.value)}
             >
               {user?.tenantIds.map((tenantId, i) => (
-                <option key={tenantId} defaultValue={user?.tenantId}>
+                <option key={tenantId} value={tenantId}>
                   {tenantId}
                 </option>
               ))}
